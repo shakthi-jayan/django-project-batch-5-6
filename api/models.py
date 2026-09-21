6db78621f7c5aa186a559a4e17da8758badfd69d
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-09-21 18:13:48 +0530

    created students model and configured it with admin

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50, null=False, primary_key= True)
+    email = models.EmailField(null=False, unique=True)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    profile_image = models.ImageField(upload_to='students/',blank=True, null=True)
+
+
+    def __str__(self):
+        return self.name
