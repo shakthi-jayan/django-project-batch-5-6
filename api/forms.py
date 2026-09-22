6fd6a163752d518fb5b83948e665160d89cd8b41
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-09-22 17:38:03 +0530

    feat: Added forms for Student Model

---
+++ api/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Student
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = [
+            "name", "email", "phone", "address", "profile_image"
+        ]
