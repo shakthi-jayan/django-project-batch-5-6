a7bb40bd1e2f6e7dab5c0cf8ab9467f47d206d72
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-09-22 18:02:26 +0530

    feat: added views to GET and POST data of student

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student
+from .forms import StudentForm
 
 # Create your views here.
+def student_list(request):
+    student = Student.objects.all()
+    return render(request, "students/list.html",{student : student})
+
+def create_student(request):
+    if request.method == "POST":
+        form = StudentForm(
+            request.POST,
+            request.FILES
+        )
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(
+        request,
+        'students/create.html',
+        {'form': form}
+    )
